06ec2377a2ad3301e25f7e548fb4027acad14e3f
Matt-Chisholm <mathesonchisholm@hotmail.com>
2022-11-26 13:01:54 -0800

    refactor with reducer

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -1,19 +1,27 @@
-import React, { useState } from "react";
+import React, { useReducer } from "react";
 
 const BlogContext = React.createContext();
 
-export const BlogProvider = ({ children }) => {
-  const [blogPosts, setBlogPosts] = useState([]);
+const blogReducer = (state, action) => {
+  switch (action.type) {
+    case "add_blogpost":
+      return [
+        ...state,
+        {
+          id: Math.floor(Math.random() * 99999),
+          title: `Blog Post #${state.length + 1}`,
+        },
+      ];
+    default:
+      return state;
+  }
+};
 
-  const addBlogPost = () => {
-    setBlogPosts([
-      ...blogPosts,
-      { title: `Blog Post #${blogPosts.length + 1}` },
-    ]);
-  };
+export const BlogProvider = ({ children }) => {
+  const [blogPosts, dispatch] = useReducer(blogReducer, []);
 
   return (
-    <BlogContext.Provider value={{ data: blogPosts, addBlogPost }}>
+    <BlogContext.Provider value={{ data: blogPosts }}>
       {children}
     </BlogContext.Provider>
   );
