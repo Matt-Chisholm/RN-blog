644e4e38b5941920206ef4d2dd21fb77a8649ad9
Matt-Chisholm <mathesonchisholm@hotmail.com>
2022-11-26 12:11:06 -0800

    set up for provider

--- App.js
+++ App.js
@@ -21,4 +21,8 @@ const navigator = createStackNavigator(
   }
 );
 
-export default createAppContainer(navigator);
+const App = createAppContainer(navigator);
+
+export default () => {
+  return <App />;
+};
