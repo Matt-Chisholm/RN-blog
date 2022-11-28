e5d2e0ee099bd8c7024a6b2c2fcbbc71a97ae555
Matt-Chisholm <mathesonchisholm@hotmail.com>
2022-11-28 11:08:33 -0800

    starter c and p

--- src/components/BlogPostForm.js
+++ src/components/BlogPostForm.js
@@ -1,12 +1,49 @@
-import { View, Text, StyleSheet } from "react-native";
-import React from "react";
+import { View, Text, StyleSheet, Button, TextInput } from "react-native";
+import React, { useState } from "react";
 
 export default function BlogPostForm({ onSubmit, initialValues }) {
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
   return (
-    <View>
-      <Text>BlogPostForm</Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>Create new blog : </Text>
+      <Text>Enter Title:</Text>
+      <TextInput
+        style={styles.input}
+        value={title}
+        onChangeText={(text) => setTitle(text)}
+      />
+      <Text>Enter Content:</Text>
+      <TextInput
+        style={styles.input}
+        value={content}
+        onChangeText={(text) => setContent(text)}
+      />
+      <Button title='Save Blog Post' />
     </View>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  input: {
+    fontSize: 18,
+    borderWidth: 1,
+    borderColor: "black",
+    marginBottom: 15,
+    margin: 10,
+    padding: 5,
+  },
+  container: {
+    flex: 1,
+    display: "flex",
+    alignitems: "center",
+    justifyContent: "center",
+    marginBottom: 250,
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: "bold",
+    textAlign: "center",
+    marginBottom: 10,
+  },
+});
