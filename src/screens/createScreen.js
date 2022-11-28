d762962297072ba518efddbac16a8d6fa9dd2059
Matt-Chisholm <mathesonchisholm@hotmail.com>
2022-11-28 09:40:52 -0800

    input styling

--- src/screens/createScreen.js
+++ src/screens/createScreen.js
@@ -1,12 +1,25 @@
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, TextInput, StyleSheet } from "react-native";
 import React from "react";
 
 export default function createScreen() {
   return (
     <View>
-      <Text>createScreen</Text>
+      <Text>Create new blog : </Text>
+      <Text>Enter Title:</Text>
+      <TextInput style={styles.input} />
+      <Text>Enter Content:</Text>
+      <TextInput style={styles.input} />
     </View>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  input: {
+    fontSize: 18,
+    borderWidth: 1,
+    borderColor: "black",
+    marginBottom: 15,
+    margin: 5,
+    padding: 5,
+  },
+});
