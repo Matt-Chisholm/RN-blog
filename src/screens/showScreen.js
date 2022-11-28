4aadc7c752843123e75479c63c7345385fbe4051
Matt-Chisholm <mathesonchisholm@hotmail.com>
2022-11-28 09:32:52 -0800

    array .find for blogpost

--- src/screens/showScreen.js
+++ src/screens/showScreen.js
@@ -1,10 +1,16 @@
 import { View, Text, StyleSheet } from "react-native";
-import React from "react";
+import React, { useContext } from "react";
+import { Context } from "../context/BlogContext";
 
-export default function showScreen() {
+export default function showScreen({ navigation }) {
+  const { state } = useContext(Context);
+  const blogPost = state.find(
+    (blogPost) => blogPost.id === navigation.getParam("id")
+  );
+
   return (
     <View>
-      <Text>showScreen</Text>
+      <Text>{blogPost.title}</Text>
     </View>
   );
 }
